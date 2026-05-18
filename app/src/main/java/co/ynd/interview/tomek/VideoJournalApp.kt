feea2cd78e555f74cfb1eaa83a814ac899c5fe9a
tdziergwa <tomasz.dziergwa@gmail.com>
2026-05-18 15:59:40 +0200

    feat(app): run OrphanFileCleaner on startup, configure backup and data extraction rules

--- app/src/main/java/co/ynd/interview/tomek/VideoJournalApp.kt
+++ app/src/main/java/co/ynd/interview/tomek/VideoJournalApp.kt
@@ -1,15 +1,26 @@
 package co.ynd.interview.tomek
 
 import android.app.Application
+import co.ynd.interview.tomek.core.data.OrphanFileCleaner
 import co.ynd.interview.tomek.core.data.di.dataModule
 import co.ynd.interview.tomek.core.database.di.databaseModule
 import co.ynd.interview.tomek.core.domain.di.domainModule
 import co.ynd.interview.tomek.feature.camera.di.cameraModule
 import co.ynd.interview.tomek.feature.feed.di.feedModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class VideoJournalApp : Application() {
+class VideoJournalApp : Application(), KoinComponent {
+
+    private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -22,5 +33,7 @@ class VideoJournalApp : Application() {
                 cameraModule
             )
         }
+        val cleaner: OrphanFileCleaner by inject()
+        appScope.launch { cleaner.clean() }
     }
 }
